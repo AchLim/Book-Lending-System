6ad7edd54e288f9ac962a736343c576cb079242f
AchLim <rimuakoru@gmail.com>
2023-01-28 12:55:32 +0700

    [FIX] Remove NPM as primary key

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Book_Lending_System.Models
 {
-    [PrimaryKey(nameof(Id), nameof(NPM))]
     public class Student
     {
         public uint Id { get; set; }
