e80e88d41cbf0a535def5460d33a42c63da34f39
AchLim <rimuakoru@gmail.com>
2023-05-28 12:49:31 +0700

    [ADD] Book Lending Status

---
+++ Data/Enum/BookLendingStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Lending_System.Data
+{
+    public enum BookLendingStatus : byte
+    {
+        [Display(Name = "Submitted")]
+        Submitted,
+
+        [Display(Name = "Approved")]
+        Approved,
+
+        [Display(Name = "Returned")]
+        Returned,
+
+        [Display(Name = "Rejected")]
+        Rejected,
+
+        [Display(Name = "Cancelled")]
+        Cancelled,
+    }
+}
