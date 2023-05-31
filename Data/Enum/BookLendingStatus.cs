ffb8a0471c9707ff034e7e9e7387d09b3952bcd3
AchLim <rimuakoru@gmail.com>
2023-06-01 03:59:24 +0700

    [ADD] Pending Payment Due status [UPDATE] Assign value for Book Lending Status enum.

--- Data/Enum/BookLendingStatus.cs
+++ Data/Enum/BookLendingStatus.cs
@@ -5,18 +5,21 @@ namespace Book_Lending_System.Data
     public enum BookLendingStatus : byte
     {
         [Display(Name = "Submitted")]
-        Submitted,
+        Submitted = 5,
 
         [Display(Name = "Approved")]
-        Approved,
+        Approved = 10,
+
+        [Display(Name = "Pending Payment Due")]
+        Pending_Payment_Due = 15,
 
         [Display(Name = "Returned")]
-        Returned,
+        Returned = 20,
 
         [Display(Name = "Rejected")]
-        Rejected,
+        Rejected = 25,
 
         [Display(Name = "Cancelled")]
-        Cancelled,
+        Cancelled = 30,
     }
 }
